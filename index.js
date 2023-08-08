409f5ea84a77f16a70e3fe36a70f20bf5bf82a97
Wonshik KIM <wokim@burninggalaxy.com>
2023-08-08 23:34:50 +0900

    feat: toggle cooling

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let accessToken = '';
 let refreshToken = '';
 let ready = false;
 let interval = 1000 * 60 * 29;
+let coolingControl = false;
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -18,45 +19,49 @@ function now() {
 
 (async () => {
   try {
-    rl.question('Please enter interval (min): ', async (answer) => {
-      const min = !answer ? 29 : parseInt(answer, 10);
-      interval = 1000 * 60 * min;
-      console.log(`[${now()}] interval: ${interval} ms`);
+    rl.question('Do you want to enable hvac toggle feature? (yes/no): ', async(answer) => {
+      if (answer === 'yes') coolingControl = true;
 
-      rl.question(`[${now()}] Do you have access/refresh token? (yes/no): `, async (answer) => {
-        if (answer.toLowerCase() === 'yes') {
-          rl.question(`[${now()}] Enter the access token and refresh token separated by a spcae:`, async (answer) => {
-            const split = answer.split(' ');
-            const [a, r] = split;
-            console.log(`Access Token: ${a}`);
-            console.log(`Refresh Token: ${r}`);
-            accessToken = a;
-            refreshToken = r;
-  
-            await refresh();
-            ready = true;
-          });
-        } else {
-          console.log(`[${now()}] Registering App. App Key is ${apiKey}`);
-          const res = await axios.get(`https://api.ecobee.com/authorize?response_type=ecobeePin&client_id=${apiKey}&scope=smartWrite`);
-  
-          rl.question(`[${now()}] Your ecobee PIN is ${res.data.ecobeePin}. Have you completed the input? (yes/no): `, async (answer) => {
-            if (answer.toLowerCase() === 'yes') {
-              const code = res.data.code;
-              console.log(`[${now()}] Requesting access token...`);
-              const xxx = await axios.post(`https://api.ecobee.com/token?grant_type=ecobeePin&code=${code}&client_id=${apiKey}&ecobee_type=jwt`);
-              accessToken = xxx.data.access_token;
-              refreshToken = xxx.data.refresh_token;
+      rl.question('Please enter interval (min): ', async (answer) => {
+        const min = !answer ? 29 : parseInt(answer, 10);
+        interval = 1000 * 60 * min;
+        console.log(`[${now()}] interval: ${interval} ms`);
+
+        rl.question(`[${now()}] Do you have access/refresh token? (yes/no): `, async (answer) => {
+          if (answer.toLowerCase() === 'yes') {
+            rl.question(`[${now()}] Enter the access token and refresh token separated by a spcae:`, async (answer) => {
+              const split = answer.split(' ');
+              const [a, r] = split;
+              console.log(`Access Token: ${a}`);
+              console.log(`Refresh Token: ${r}`);
+              accessToken = a;
+              refreshToken = r;
+    
+              await refresh();
               ready = true;
-              console.log(`[${now()}] Access token: ${accessToken}`);
-              console.log(`[${now()}] Refresh token: ${refreshToken}`);
-              console.log(`[${now()}] Token acquired. Ready to go!`);
-            } else {
-              console.log(`[${now()}] You must enter PIN. Terminating app...`);
-              return;
-            }
-          });
-        }
+            });
+          } else {
+            console.log(`[${now()}] Registering App. App Key is ${apiKey}`);
+            const res = await axios.get(`https://api.ecobee.com/authorize?response_type=ecobeePin&client_id=${apiKey}&scope=smartWrite`);
+    
+            rl.question(`[${now()}] Your ecobee PIN is ${res.data.ecobeePin}. Have you completed the input? (yes/no): `, async (answer) => {
+              if (answer.toLowerCase() === 'yes') {
+                const code = res.data.code;
+                console.log(`[${now()}] Requesting access token...`);
+                const xxx = await axios.post(`https://api.ecobee.com/token?grant_type=ecobeePin&code=${code}&client_id=${apiKey}&ecobee_type=jwt`);
+                accessToken = xxx.data.access_token;
+                refreshToken = xxx.data.refresh_token;
+                ready = true;
+                console.log(`[${now()}] Access token: ${accessToken}`);
+                console.log(`[${now()}] Refresh token: ${refreshToken}`);
+                console.log(`[${now()}] Token acquired. Ready to go!`);
+              } else {
+                console.log(`[${now()}] You must enter PIN. Terminating app...`);
+                return;
+              }
+            });
+          }
+        });
       });
     });
   } catch (error) {
@@ -86,6 +91,58 @@ setInterval(async () => {
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+setInterval(async () => {
+  if (!ready) return;
+  if (!coolingControl) return;
+  
+  try {
+    console.log(`[${now()}] Try to turn off cooling`);
+    await axios.post(`https://api.ecobee.com/1/thermostat?format=json`, {
+      "selection": {
+          "selectionType":"registered",
+          "selectionMatch":""
+      },
+      "thermostat":{
+          "settings": {
+              "hvacMode": "off"
+          }
+      }
+    }, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'Content-Type': 'application/json'
+      }
+    });
+    console.log(`[${now()}] cooling turned off`);
+
+    console.log(`[${now()}] Sleep 1 min...`);
+    await sleep(1 * 60 * 1000);
+
+    console.log(`[${now()}] Try to turn on cooling`);
+    await axios.post(`https://api.ecobee.com/1/thermostat?format=json`, {
+      "selection": {
+          "selectionType":"registered",
+          "selectionMatch":""
+      },
+      "thermostat":{
+          "settings": {
+              "hvacMode": "cool"
+          }
+      }
+    }, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'Content-Type': 'application/json'
+      }
+    });
+    console.log(`[${now()}] cooling turned on`);
+
+  } catch (e) {
+    console.log(`[${now()}] Failed to control hvac. Terminating app...`);
+    console.log(e);
+  }
+}, interval);
 
 setInterval(async () => {
   if (!ready) return;
